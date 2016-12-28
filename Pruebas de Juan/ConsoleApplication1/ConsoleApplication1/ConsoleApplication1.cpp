10f0c4ac534a8ab40baf7e301fa8c2a028c3853f
JuanFdelArco <juanfdelarco@gmail.com>
2016-12-28 22:41:16 +0100

    Codigo de pruebas implementado
    Gracias a este mini-codigo, estoy 100% seguro de que lo que nos peta el
    programa es la funcion de encontrar contornos.

--- Pruebas de Juan/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Pruebas de Juan/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include <iostream>
-
 #include <opencv\cv.hpp>
+#include <vector>
 
 using namespace std;
 using namespace cv;
@@ -11,20 +11,28 @@ Mat image;
 int main()
 {
 	VideoCapture cap(0);
-	Mat frame,grey;
+	Mat frame, grey_img, blur_img, bin_img, bin_img2;
+	Rect roi(140, 100, 270, 270);
+	vector<vector<Point>> contornos;
 
 	for (;;)
 	{		
 		cap >> frame;
-				
+
 		if (frame.empty())
 			break;
 
-		frame.copyTo(image);
-		cvtColor(image, grey, CV_BGR2GRAY);
+		image.create(frame.rows,frame.cols,CV_8UC1);
+		image = frame(roi);
+		cvtColor(image, grey_img, CV_BGR2GRAY);
+		blur(grey_img, blur_img, Size(3, 3));
+		threshold(blur_img, bin_img, 0, 255, THRESH_BINARY_INV + THRESH_OTSU);
+		bin_img.convertTo(bin_img2, CV_8U);
 
-		imshow("Demo", grey);
-	
+		findContours(bin_img2,contornos,CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cv::Point(0,0)); //Voy a destruir al p*** creador de esta funci¨®n
+
+		imshow("Demo", bin_img);
+
 		char c = (char)waitKey(10);
 		if (c == 27)
 			break;		
