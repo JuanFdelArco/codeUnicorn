c37973bd39144964f416286b1189d946635acd36
JuanFdelArco <juanfdelarco@gmail.com>
2016-12-28 22:49:40 +0100

    Cambiada area de la region de interés

--- Pruebas de Juan/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Pruebas de Juan/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -11,8 +11,8 @@ Mat image;
 int main()
 {
 	VideoCapture cap(0);
-	Mat frame, grey_img, blur_img, bin_img, bin_img2;
-	Rect roi(140, 100, 270, 270);
+	Mat frame, grey_img, blur_img, bin_img, canny_img;
+	Rect roi(140, 100, 200, 200);
 	vector<vector<Point>> contornos;
 
 	for (;;)
@@ -27,11 +27,11 @@ int main()
 		cvtColor(image, grey_img, CV_BGR2GRAY);
 		blur(grey_img, blur_img, Size(3, 3));
 		threshold(blur_img, bin_img, 0, 255, THRESH_BINARY_INV + THRESH_OTSU);
-		bin_img.convertTo(bin_img2, CV_8U);
+		Canny(bin_img, canny_img, 50, 100, 3);
 
-		findContours(bin_img2,contornos,CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cv::Point(0,0)); //Voy a destruir al p*** creador de esta funci¨®n
+		//findContours(canny_img,contornos,CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cv::Point(0,0)); //Voy a destruir al p*** creador de esta funci¨®n
 
-		imshow("Demo", bin_img);
+		imshow("Demo", canny_img);
 
 		char c = (char)waitKey(10);
 		if (c == 27)
