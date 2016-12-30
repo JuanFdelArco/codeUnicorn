096585176a448c5dabcf1ba233f2e26dbbecd33c
JuanFdelArco <juanfdelarco@gmail.com>
2016-12-30 17:18:20 +0100

    He añadido el reconocimiento de contornos
    He añadido la versión de Mar de la detección de contornos al código sin captura para que alguna lo pruebe a ver si le va.

--- Pruebas de Juan/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Pruebas de Juan/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -13,7 +13,8 @@ int main()
 	VideoCapture cap(0);
 	Mat frame, grey_img, blur_img, bin_img, canny_img;
 	Rect roi(140, 100, 200, 200);
-	vector<vector<Point>> contornos;
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
 
 	for (;;)
 	{		
@@ -29,9 +30,54 @@ int main()
 		threshold(blur_img, bin_img, 0, 255, THRESH_BINARY_INV + THRESH_OTSU);
 		Canny(bin_img, canny_img, 50, 100, 3);
 
-		//findContours(canny_img,contornos,CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cv::Point(0,0)); //Voy a destruir al p*** creador de esta funci¨®n
+		findContours(canny_img,contours,CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cv::Point(0,0)); //Voy a destruir al p*** creador de esta funciÃ³n
 
-		imshow("Demo", canny_img);
+		int c_size = 0, c_idx = 0;
+		Mat drawing = image.clone();
+		for (int i = 0; i < contours.size(); i++) {
+			cout << contours[i] << endl;
+			if (contours.size() > c_size) { 
+				c_idx = i;
+				c_size = contours.size();
+			}
+			if (i != 0) {	//Dibujamos el contorno, quitamos el primer valor porque es el recuadro de la imagen
+				drawContours(drawing, contours, i, (0, 0, 255), 2, 8, hierarchy, 0, Point());
+			}
+		}
+
+		//Pasamos el mayor valor al vector boundary0 (es el recté†¤gulo)
+		vector<Point> boundary0;
+		//Acumulamos el vector que se dibuja en una nueva variable
+		vector<Point> boundary1;
+		vector<Point> peaks_;
+		if (!contours.empty()) {
+			boundary0 = contours[c_idx]; //cnt
+			boundary1 = contours[1];
+			cout << boundary0 << endl;
+		}
+
+		//ESTO NO FUNCIONA
+		//Aproximamos el contorno y lo dibujamos
+		vector<Point> boundary;
+		if (!boundary0.empty()) {
+			approxPolyDP(boundary1, boundary, 2, false); //Aproxima el valor del contorno
+
+			for (int i = 0; i < boundary.size(); i++) {
+				cout << "Estoy dibujando" << endl;
+				cout << (boundary[i] + Point(roi.x, roi.y)) << endl;
+				Point center = boundary[i] + Point(roi.x, roi.y);
+				circle(image, center, 25, Scalar(0, 255, 0));
+				//imshow("Contornos", temp);
+			}
+		}
+		/// Show in a window
+
+		imshow("Contours", drawing);
+		imshow("Resultado", image);
+
+
+
+		//imshow("Demo", canny_img);
 
 		char c = (char)waitKey(10);
 		if (c == 27)
